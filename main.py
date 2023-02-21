1c49f52a070943445ca70718330af358606a3932
GauravSingh02 <125949909+GauravSingh02@users.noreply.github.com>
2023-02-21 11:28:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome! Gaurav")
+name = "Himani"
+age = 32
+b=input("What is your name ?")
+print("Entered Name is " +b)
+print( name + " having age is " + str(age)) // #Cancatination means convert one Type Integer to Others
+
